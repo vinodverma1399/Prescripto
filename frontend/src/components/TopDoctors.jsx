635ce34306842e8bfcf5de5c0de8eb364156661c
vinodverma1399 <vinodverma2555@gmail.com>
2025-12-22 00:59:08 +0530

    Add Tailwind CSS v3 setup and update components and top doctors ui complete

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { doctors } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const TopDoctors = () => {
+  const navigate=useNavigate();
   return (
     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
         <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
         <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors.</p>
-        <div className='w-full grid grid-col-auto gap-4 gap-y-6 px-3 sm:px-0'>
+        <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
           {
             doctors.slice(0,10).map((item,index)=>(
-              <div className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
+              <div onClick={()=>navigate(`/appointment/${item._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
                 <img className='bg-blue-50' src={item.image} alt=''/>
                   <div className='p-4'>
                     <div className='flex items-center gap-2 text-sm text-center text-green-500'>
@@ -24,7 +26,7 @@ const TopDoctors = () => {
             ))
           }
         </div>
-        <button>more</button>
+        <button className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10'>more</button>
     </div>
   )
 }
