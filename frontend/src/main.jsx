022764405fd21b87753eff121b869b5c948168fa
Vinod Kumar <vinodverma2555@gmail.com>
2025-12-26 01:46:55 +0530

    Context componenet add and modifiy

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import AppContextProvider from './context/AppContext'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <AppContextProvider>
+      <App />
+    </AppContextProvider> 
   </BrowserRouter>
 )
